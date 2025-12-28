33defc945e5b921561b54cca5f384d4d0dfe8126
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2025-12-28 10:46:20 +0800

    auto: 2025-12-28-10:46

--- modules/home/programs/niri.nix
+++ modules/home/programs/niri.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  xdg.configFile."niri/config.kdl".text = ''
-    spawn-at-startup "qs" "-c" "noctalia-shell"
-  '';
+  # xdg.configFile."niri/config.kdl".text = ''
+  #   spawn-at-startup "qs" "-c" "noctalia-shell"
+  # '';
 }
